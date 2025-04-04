a1ff1ea2a2506fd5973ebf25235b23acb3f5b1ef
Marcelloax <marcellomachado2014@gmail.com>
2025-04-03 22:01:51 -0300

    ATIVIDADERAFA

---
+++ Atividadesbeeclowd/Atividadesbeeclowd/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+int HORAS = int.Parse(Console.ReadLine());
+int VALOR = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+int SALARIO = HORAS * VALOR;
+Console.WriteLine("SALARIO = U$ " + SALARIO.ToString("F2",CultureInfo.InvariantCulture));
+
